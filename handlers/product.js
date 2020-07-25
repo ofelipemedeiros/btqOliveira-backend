e8b75e3e32c1d40db90559ea0b02ddd9218f9c21
Felipe Medeiros <ofelipemedeiros@hotmail.com>
2020-07-25 09:48:14 -0300

    Criado o insert/save no handlers para adicionar produtos

--- handlers/product.js
+++ handlers/product.js
@@ -20,6 +20,22 @@ const find = async (req, h) => {
 }
 
 const save = async(req, h) => {
+    const  {nome, descricao, dataEntrada, categ, provider} = req.payload
+    const produto = {
+        nome: nome,
+        descricao: descricao,
+        dataEntrada: dataEntrada,
+        categ: categ,
+        provider: provider
+    }
+
+    try {
+        await database.insert(produto).into("produto")
+        return h.response(produto).code(201)
+    } catch (error) {
+        console.log(error);
+        
+    }
 
 }
 
